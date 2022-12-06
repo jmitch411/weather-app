9374cd2c55e1f349cfacccbfdc0832fb0cd70ec2
jmitch411 <mitchelljasmin15@yahoo.com>
2022-12-06 10:22:05 -0500

    Added accurate icons from the API

--- src/script.js
+++ src/script.js
@@ -24,6 +24,11 @@ function displayWeatherCondition(response) {
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
   document.querySelector("#wind").innerHTML = Math.round(response.data.wind.speed);
   document.querySelector("#description").innerHTML = response.data.weather[0].main;
+  let iconElement = document.querySelector("#icon");
+
+  iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+
+  iconElement.setAttribute("alt", response.data.weather[0].icon);
 }
 
 function searchCity(city) {
